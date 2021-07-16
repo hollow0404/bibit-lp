4356ff9163634f4a2f98acd624db7d540cb7c78e
kyoto-library <kyoto.library59@gmail.com>
2021-07-16 17:44:33 +0900

    create header

--- header.php
+++ header.php
@@ -5,9 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/index.css" />
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,300&display=swap" rel="stylesheet">
     <title>一人で悩まない婚活 | BiBit</title>
     <?php wp_head(); ?>
   </head>
   <body>
     <header>
+      <div class="header-container">
+        <div class="header-container__first">
+          <div class="header-container__left">
+            <img class="header-container__logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="ロゴ画像">
+            <div class="header-container__appeal is-pc">
+              <i class="fas fa-car"></i>
+              <div class="header-container__appeal-text">お近くへ出張相談いたします</div>
+              <div class="header-container__appeal-label">無料</div>
+            </div>
+          </div>
+          <div class="header-container__right">
+            <div class="header-container__time-zone">         
+              受付時間 10:00 ~ 19:00
+            </div>
+            <div class="header-container__call">
+              <i class="fas fa-phone"></i>
+              <a href="tel:077-576-6627" class="header-container__phone-number">
+                077-576-6627
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="header-container__second is-sp">
+          <div class="header-container__appeal">
+            <i class="fas fa-car"></i>
+            <div class="header-container__appeal-text">お近くへ出張相談いたします</div>
+            <div class="header-container__appeal-label">無料</div>
+          </div>
+        </div>
+      </div>
     </header>
--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php get_header(); ?>
+
+<main>
+  <section>
+    <div class="main-visual">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/main-visual.png" alt="メイン画像" />
+    </div>
+  </section>
+</main>
+
 <?php get_footer(); ?>
