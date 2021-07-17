9445a803dfd34ed8395f38a725709edeb9a36674
kyoto-library <kyoto.library59@gmail.com>
2021-07-17 19:52:22 +0900

    create cv-point

--- index.php
+++ index.php
@@ -34,6 +34,19 @@
       </div>
     </div>
   </section>
+  <section>
+    <div class="cv-point">
+      <div class="cv-point__text">
+        \ まずはこちら、対面でもオンラインでもOK! /
+      </div>
+      <div class="cv-point__button">
+        <a href="#" class="cv-point__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/start-btn.png" alt="ボタン" />
+          <span>無料相談を予約する</span>
+        </a>
+      </div>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
