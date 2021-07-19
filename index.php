4a0dfe39091938829774e1b8df62f760a849cfd7
kyoto-library <kyoto.library59@gmail.com>
2021-07-20 00:06:53 +0900

    create movie

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <main>
   <?php include("mv.php"); ?>
+  <?php include("movie.php"); ?>
   <?php include("cv.php"); ?>
   <?php include("intro.php"); ?>
 </main>
---
+++ movie.php
@@ -0,0 +1,11 @@
+<section>
+  <div class="sec-title">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/frame.png" alt="フレーム" />
+    <div class="sec-title__text">
+      パートナー紹介
+    </div>
+  </div>
+  <div class="movie">
+    <video src="<?php echo get_template_directory_uri(); ?>/video/pr-movie.mp4" controls controlsList="nodownload" poster="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png"></video>
+  </div>
+</section>
