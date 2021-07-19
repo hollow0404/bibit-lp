e974b6c9f25bb42df239c10dd6d25f1479b88dd2
kyoto-library <kyoto.library59@gmail.com>
2021-07-20 00:15:18 +0900

    create graph

---
+++ graph.php
@@ -0,0 +1,6 @@
+<section>
+  <div class="graph">
+    <img class="is-pc" src="<?php echo get_template_directory_uri(); ?>/img/graph.png" alt="グラフ" />
+    <img class="is-sp" src="<?php echo get_template_directory_uri(); ?>/img/graph-sp.png" alt="グラフ" />
+  </div>
+</section>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <?php include("movie.php"); ?>
   <?php include("cv.php"); ?>
   <?php include("intro.php"); ?>
+  <?php include("graph.php"); ?>
 </main>
 
 <?php get_footer(); ?>
