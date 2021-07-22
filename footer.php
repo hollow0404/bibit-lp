e1cc305354512ed20e1341285072fdae4c79170b
kyoto-library <kyoto.library59@gmail.com>
2021-07-22 17:29:11 +0900

    create footer

--- footer.php
+++ footer.php
@@ -1,4 +1,25 @@
 <?php wp_footer(); ?> 
-<?php wp_footer(); ?> 
+
+    <footer>
+      <div class="footer-container">
+        <img class="is-pc" src="<?php echo get_template_directory_uri(); ?>/img/footer.png" alt="背景"/>
+        <img class="is-sp" src="<?php echo get_template_directory_uri(); ?>/img/footer-sp.png" alt="背景"/>
+        <div class="footer-container__contents">
+          <div class="footer-container__address">
+            <p>事業所：滋賀県大津市</p>
+            <p>面談オフィス：滋賀県彦根市野田山町901-2</p>
+          </div>
+          <div class="footer-container__number">
+            <a href="tel:077-576-6627">
+              077-576-6627
+            </a>
+          </div>
+          <small>
+            ©<?php echo date('Y'); ?> BiBit, All rights reserved
+          </small>
+        </div>
+      </div>
+    </footer>
+
   </body>
 </html>
--- movie.php
+++ movie.php
@@ -7,5 +7,6 @@
   </div>
   <div class="movie">
     <video src="<?php echo get_template_directory_uri(); ?>/video/pr-movie.mp4" controls controlsList="nodownload" poster="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png"></video>
+    <p>※撮影：面談オフィス</p>
   </div>
 </section>
