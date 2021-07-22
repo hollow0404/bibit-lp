e5045f5ef3c4efe999f451957ccb3aeee9ecd88a
kyoto-library <kyoto.library59@gmail.com>
2021-07-22 19:14:11 +0900

    Update movie.php

--- movie.php
+++ movie.php
@@ -6,7 +6,7 @@
     </div>
   </div>
   <div class="movie">
-    <video src="<?php echo get_template_directory_uri(); ?>/video/pr-movie.mp4" controls controlsList="nodownload" poster="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png"></video>
+    <video src="http://lp.bo-bibit.com/wp-content/uploads/2021/07/pr-movie.mp4" controls controlsList="nodownload" poster="<?php echo get_template_directory_uri(); ?>/img/thumbnail.png"></video>
     <p>※撮影：面談オフィス</p>
   </div>
 </section>
