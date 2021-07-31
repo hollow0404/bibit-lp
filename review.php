21a9bc95613dade10c3f7d420bf85e4ca77bea08
kyoto-library <kyoto.library59@gmail.com>
2021-07-31 17:07:05 +0900

    Update review.php

--- review.php
+++ review.php
@@ -10,6 +10,21 @@
       <div class='c-card-review-lg__blocks owl-carousel owl-theme'>
         <div class='c-card-review-lg__block' >
           <div class='c-card-review-lg__image'>
+            <img id="customer_review_image" src="<?php echo get_template_directory_uri(); ?>/img/review2.jpg" alt="会員様の声"/>
+          </div>
+          <div class='c-card-review-lg__content'>
+            <div class='c-card-review-lg__inner'>
+              <p class="c-card-review-lg__title">50代女性 Mさん</p>
+              <div class='c-card-review-lg__text'>
+                <p>
+                  2020年12月に会社の同僚からの勧めもあり、入会させていただきました。<br>結婚の相談もですが、心配事もすぐに聞いていただき本当に頼りがいのある方です。何人かお見合いをする中で、4月から交際をスタートさせていただき、自分でもビックリするぐらい交際が続いて行きました。3ヶ月に入る頃、真剣交際に入るか尋ねられ、お相手の方や森田さんともじっくり話をして真剣交際に突入しました。森田さんの助言がなかったら進んでいなかったと思います。<br>そして今月7月にプロポーズをしてもらい、お受けしました。まさか私が結婚できるなんて思ってもいなかったので、自分が一番ビックリしています。あの時思い切って入会して本当に良かったと同僚にも感謝しています。成婚退会した後も、気軽に連絡してねと言って頂き、心強く思っています。これから何でも二人で話し合い、笑顔の絶えない些細な事を幸せと感じられる、そんな家庭をつくっていきます。本当にありがとうございました。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class='c-card-review-lg__block' >
+          <div class='c-card-review-lg__image'>
             <img id="customer_review_image" src="<?php echo get_template_directory_uri(); ?>/img/review1.jpg" alt="会員様の声"/>
           </div>
           <div class='c-card-review-lg__content'>
@@ -19,21 +34,6 @@
                 <p>
                   ありきたりですが、独身貴族を謳歌していた仲間がみんな結婚していき、自分も結婚したいと思ったのですが、職場は殆どが男性で出会いもないので、出会いを求めて入会しました。<br>
                   活動を始めて一番最初に感じたのは、活動されてる人の多さですね。始めて4ヶ月ですが、幸運にもコンスタントに色々な人にお会いすることが出来ており「こんなにも出会うチャンスがあるのか！もっと早くに始めたら良かった！」とビックリしてるのが正直な感想です。<br>専任のパートナーさんがおられるので、お断りするときや、お見合いの日程調整など、全てしてくださるので、ストレスがなく、自分はお相手だけを見て活動できるのが結婚相談所の最大のメリットだと思います。<br>現在は一人の女性と交際中です。これからデートを重ねて相手のことを知っていければと思っています。
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class='c-card-review-lg__block' >
-          <div class='c-card-review-lg__image'>
-            <img id="customer_review_image" src="<?php echo get_template_directory_uri(); ?>/img/review2.jpg" alt="会員様の声"/>
-          </div>
-          <div class='c-card-review-lg__content'>
-            <div class='c-card-review-lg__inner'>
-              <p class="c-card-review-lg__title">50代女性 Mさん</p>
-              <div class='c-card-review-lg__text'>
-                <p>
-                  昨年の12月に会社の同僚からの勧めもあり、入会させていただきました。結婚の相談もですが、心配事もすぐに聞いていただき本当に頼りがいのある方です。<br>今は真剣交際させていただいている方がいますが、森田さんの助言がなかったら進んでいなかったと思います。日々感謝でいっぱいです。
                 </p>
               </div>
             </div>
