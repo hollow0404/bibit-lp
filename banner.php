eb67d8ef11ae3e787bd85940d19639ba71484189
kyoto-library <kyoto.library59@gmail.com>
2022-05-19 14:54:50 +0900

    change_banner

--- banner.php
+++ banner.php
@@ -1,7 +1,7 @@
 <section>
   <div class="banner">
     <a href="#campaign">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/banner.png" alt="キャンペーン" />
+      <img src="<?php echo get_template_directory_uri(); ?>/img/banner2.png" alt="キャンペーン" />
     </a>
   </div>
 </section>
