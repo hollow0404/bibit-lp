b49c8d29775f5a2363e3f7498156dbd8bd699e52
kyoto-library <kyoto.library59@gmail.com>
2022-10-09 17:24:58 +0900

    Update sns.php

--- sns.php
+++ sns.php
@@ -4,7 +4,7 @@
   </div>
 </a>
 
-<a href="https://www.facebook.com/profile.php?id=100056759657222" target="_blank">
+<a href="https://instagram.com/maho_bibit?r=nametag" target="_blank">
   <div class="sns2">
     <i class="fab fa-instagram"></i>
   </div>
